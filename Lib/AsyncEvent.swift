613a939ca4bd7ef37efd8384077e1bb853c5ea83
vlg <gorbenko.vova@gmail.com>
2016-02-05 11:13:29 +0300

    implementing reactiveKit additions

--- Lib/AsyncEvent.swift
+++ Lib/AsyncEvent.swift
@@ -18,276 +18,77 @@ public enum AsyncEvent<ValueT, ProgressT, ErrorT: ErrorType> {
     case Success(ValueT)
     case Failure(ErrorT)
     case Progress(ProgressT)
+
+    public var isTerminal: Bool {
+        switch self {
+        case .Success, .Failure:
+            return true
+        case .Progress:
+            return false
+        }
+    }
 }
 
 public protocol AsyncStreamType: StreamType {
     typealias Value
     typealias Progress
     typealias Error: ErrorType
-    typealias Event = AsyncEvent<Value, Progress, Error>
+
+    func lift<R, P, E: ErrorType>(transform: Stream<AsyncEvent<Value, Progress, Error>> -> Stream<AsyncEvent<R, P, E>>) -> AsyncStream<R, P, E>
 }
 
-extension AsyncStreamType where Event == AsyncEvent<Value, Progress, Error> {
+public struct AsyncStream<ValueT, ProgressT, ErrorT: ErrorType>: AsyncStreamType {
 
-    public func mapValue<U>(transform: Value -> U) -> Stream<AsyncEvent<U, Progress, Error>> {
-        return create { observer in
-            return self.observe(on: nil) { event in
+    public typealias Value    = ValueT
+    public typealias Progress = ProgressT
+    public typealias Error    = ErrorT
+    public typealias Event    = AsyncEvent<Value, Progress, Error>
 
-                switch event {
-                case .Success(let value):
-                    observer(.Success(transform(value)))
-                case .Failure(let error):
-                    observer(.Failure(error))
-                case .Progress(let progress):
-                    observer(.Progress(progress))
-//                case .Interrupted:
-//                    observer(.Interrupted)
-//                case .Unsubscribed:
-//                    observer(.Unsubscribed)
-                }
-            }
-        }
-    }
+    private let stream: Stream<Event>
 
-    public func mapError<U>(transform: Error -> U) -> Stream<AsyncEvent<Value, Progress, U>> {
-        return create { observer in
-            return self.observe(on: nil) { event in
+    public typealias Observer = Event -> ()
+    
+    public init(producer: (Observer -> DisposableType?)) {
+        stream = Stream  { observer in
+            var completed: Bool = false
 
-                switch event {
-                case .Success(let value):
-                    observer(.Success(value))
-                case .Failure(let error):
-                    observer(.Failure(transform(error)))
-                case .Progress(let progress):
-                    observer(.Progress(progress))
-//                case .Interrupted:
-//                    observer(.Interrupted)
-//                case .Unsubscribed:
-//                    observer(.Unsubscribed)
-                }
-            }
-        }
-    }
-
-    public func mapProgress<U>(transform: Progress -> U) -> Stream<AsyncEvent<Value, U, Error>> {
-        return create { observer in
-            return self.observe(on: nil) { event in
-
-                switch event {
-                case .Success(let value):
-                    observer(.Success(value))
-                case .Failure(let error):
-                    observer(.Failure(error))
-                case .Progress(let progress):
-                    observer(.Progress(transform(progress)))
-//                case .Interrupted:
-//                    observer(.Interrupted)
-//                case .Unsubscribed:
-//                    observer(.Unsubscribed)
-                }
-            }
-        }
-    }
-
-    public func unsubscribe() -> Stream<Event> {
-
-        return create { observer in
-
-            var disposed = false
-
-            self.observe(on: nil) { event in
-                if !disposed {
+            return producer { event in
+                if !completed {
+                    completed = event.isTerminal
                     observer(event)
                 }
             }
-
-            return BlockDisposable({ () -> () in
-                disposed = true
-            })
         }
     }
 
-    //TODO test
-    public func withEventValue(getter: () -> Event?, setter: Event -> Void) -> Stream<Event> {
+    public func observe(on context: ExecutionContext? = ImmediateOnMainExecutionContext, observer: Observer) -> DisposableType {
+        return stream.observe(on: context, observer: observer)
+    }
 
+    public static func succeeded(with value: Value) -> Operation<Value, Error> {
         return create { observer in
-
-            let event = getter()
-
-            if let event = event {
-
-                observer(event)
-                return nil
-            }
-
-            return self.observe(on: nil) { event in
-
-                setter(event)
-                observer(event)
-            }
+            observer.next(value)
+            observer.success()
+            return nil
         }
     }
 
-    //TODO test
-    public func mergedObservers() -> Stream<Event> {
-
-        var observers: [(Event -> ())?] = []
-
+    public static func failed(with error: Error) -> Operation<Value, Error> {
         return create { observer in
-
-            observers.append(observer)
-
-            if observers.count > 1 {
-
-                let index = observers.count - 1
-                return BlockDisposable({ () -> () in
-
-                    observers[index] = nil
-                })
-            }
-
-            let notify = { (observers: [(Event -> ())?], event: Event) in
-                observers.forEach { $0?(event) }
-            }
-            let finishNotify = { (event: Event) in
-                let observers_ = observers
-                observers.removeAll()
-                notify(observers_, event)
-            }
-
-            let dispose = self.observe(on: nil) { event in
-
-                switch event {
-                case .Success:
-                    finishNotify(event)
-                case .Failure:
-                    finishNotify(event)
-//                case .Interrupted:
-//                    finishNotify(event)
-//                case .Unsubscribed:
-//                    finishNotify(event)
-                case .Progress:
-                    notify(observers, event)
-                }
-            }
-
-            return BlockDisposable({ () -> () in
-
-                observers[0] = nil
-                if observers.all({ $0 == nil }) {
-                    dispose.dispose()
-                }
-            })
+            observer.failure(error)
+            return nil
         }
     }
 
-    public func ensureOnceStreamFinish() -> Stream<Event> {
-
-        return Stream(producer: { (observer: Event -> ()) -> DisposableType? in
-
-            var finished = false
-            let callObserverOnce = { (event: Event) -> Void in
-                if !finished {
-                    finished = true
-                    observer(event)
-                }
-            }
-
-            let dispose = self.observe(on: nil, observer: { (event) -> () in
-                
-                switch event {
-                case .Success, .Failure://, .Interrupted, .Unsubscribed:
-                    callObserverOnce(event)
-                case .Progress:
-                    observer(event)
-                }
-            })
-            
-            return dispose
-        })
+    public func lift<R, P, E: ErrorType>(transform: Stream<AsyncEvent<Value, Progress, Error>> -> Stream<AsyncEvent<R, P, E>>) -> AsyncStream<R, P, E> {
+        return create { observer in
+            return transform(self.stream).observe(on: nil, observer: observer)
+        }
     }
 }
 
-public func asyncToStream<Value, Error: ErrorType>(loader: AsyncTypes<Value, Error>.Async) -> Stream<AsyncEvent<Value, AnyObject, Error>> {
-
-    typealias Event = AsyncEvent<Value, AnyObject, Error>
-
-    let result = Stream { (observer: Event -> ()) -> DisposableType? in
-
-        let handler = loader(progressCallback: { (progressInfo) -> () in
-
-            observer(.Progress(progressInfo))
-        }, stateCallback: { (state) -> () in
-
-            //ignore, still not used
-            fatalError()
-        }, finishCallback: { (result) -> Void in
-
-            switch result {
-            case .Success(let value):
-                observer(.Success(value))
-            case .Failure(let error):
-                observer(.Failure(error))
-            case .Interrupted:
-                break
-            case .Unsubscribed:
-                break
-            }
-        })
-
-        return BlockDisposable({ () -> () in
-
-            handler(task: .Cancel)
-        })
-    }
-
-    return result
-}
-
-public func streamToAsync<Input: AsyncStreamType where
-    Input.Progress == AnyObject,
-    Input.Event == AsyncEvent<Input.Value, AnyObject, Input.Error>>(input: Input) -> AsyncTypes<Input.Value, Input.Error>.Async {
-
-    return { (
-        progressCallback: AsyncProgressCallback?,
-        stateCallback   : AsyncChangeStateCallback?,
-        finishCallback  : AsyncTypes<Input.Value, Input.Error>.DidFinishAsyncCallback?) -> AsyncHandler in
-
-        var finishCallbackHolder = finishCallback
-        let finishOnce = { (result: AsyncResult<Input.Value, Input.Error>) -> Void in
-            if let finishCallback = finishCallbackHolder {
-                finishCallbackHolder = nil
-                finishCallback(result: result)
-            }
-        }
-
-        let dispose = input.ensureOnceStreamFinish().observe(on: nil, observer: { event -> () in
-
-            if finishCallbackHolder == nil { return }
-
-            switch event {
-            case .Success(let value):
-                finishOnce(.Success(value))
-            case .Failure(let error):
-                finishOnce(.Failure(error))
-            case .Progress(let progress):
-                progressCallback?(progressInfo: progress)
-            }
-        })
-
-        return { (task: AsyncHandlerTask) -> Void in
-
-            switch task {
-            case .Cancel:
-                dispose.dispose()
-                finishOnce(.Interrupted)
-            case .UnSubscribe:
-                finishOnce(.Unsubscribed)
-            case .Resume:
-                fatalError()
-            case .Suspend:
-                fatalError()
-            }
-        }
+public func create<Value, Progress, Error: ErrorType>(producer producer: (AsyncEvent<Value, Progress, Error> -> ()) -> DisposableType?) -> AsyncStream<Value, Progress, Error> {
+    return AsyncStream<Value, Progress, Error> { observer in
+        return producer(observer)
     }
 }
