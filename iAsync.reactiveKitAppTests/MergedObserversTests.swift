077c243c36d86a14a84aa9d1599a58d88d04bdcc
vlg <gorbenko.vova@gmail.com>
2016-02-05 12:35:24 +0300

    implementing reactiveKit additions

--- iAsync.reactiveKitAppTests/MergedObserversTests.swift
+++ iAsync.reactiveKitAppTests/MergedObserversTests.swift
@@ -86,12 +86,154 @@ class MergedObserversTests: XCTestCase {
     }
 
     func testTwoObservsersDisposeFirst() {
+
+        let stream = testStream().mergedObservers()
+
+        var progressCalledCount1 = 0
+        var resultValue1: String?
+
+        var deinitTest1: NSObject? = NSObject()
+        weak var weakDeinitTest1 = deinitTest1
+
+        let dispose1 = stream.observe { ev -> Void in
+
+            switch ev {
+            case .Success(let value):
+                if deinitTest1 != nil {
+                    deinitTest1 = nil
+                    resultValue1 = value
+                }
+            case .Failure:
+                XCTFail()
+            case .Progress(let progress):
+                XCTAssertEqual(progressCalledCount1, progress as? Int)
+                progressCalledCount1 += 1
+            }
+        }
+
+        var progressCalledCount2 = 0
+        var resultValue2: String?
+
+        var deinitTest2: NSObject? = NSObject()
+        weak var weakDeinitTest2 = deinitTest2
+
+        let expectation2 = expectationWithDescription("")
+
+        stream.observe { ev -> Void in
+
+            switch ev {
+            case .Success(let value):
+                if deinitTest2 != nil {
+                    deinitTest2 = nil
+                    resultValue2 = value
+                    expectation2.fulfill()
+                }
+            case .Failure:
+                XCTFail()
+            case .Progress(let progress):
+                XCTAssertEqual(progressCalledCount2, progress as? Int)
+                progressCalledCount2 += 1
+            }
+        }
+
+        XCTAssertNotNil(weakDeinitTest1)
+        XCTAssertNotNil(weakDeinitTest2)
+
+        dispose1.dispose()
+        deinitTest1 = nil
+
+        waitForExpectationsWithTimeout(0.5, handler: nil)
+
+        XCTAssertNil(weakDeinitTest1)
+        XCTAssertNil(weakDeinitTest2)
+
+        XCTAssertEqual(0, progressCalledCount1)
+        XCTAssertEqual(5, progressCalledCount2)
+        XCTAssertNil(resultValue1)
+        XCTAssertEqual("ok", resultValue2)
+
+        XCTAssertEqual(1, numberOfObservers)
+    }
+
+    func testTwoObservsersDisposeFirstImmediately() {
+
+        let stream = testStream().mergedObservers()
+
+        var progressCalledCount1 = 0
+        var resultValue1: String?
+
+        var deinitTest1: NSObject? = NSObject()
+        weak var weakDeinitTest1 = deinitTest1
+
+        let dispose1 = stream.observe { ev -> Void in
+
+            switch ev {
+            case .Success(let value):
+                if deinitTest1 != nil {
+                    deinitTest1 = nil
+                    resultValue1 = value
+                }
+            case .Failure:
+                XCTFail()
+            case .Progress(let progress):
+                XCTAssertEqual(progressCalledCount1, progress as? Int)
+                progressCalledCount1 += 1
+            }
+        }
+
+        dispose1.dispose()
+        deinitTest1 = nil
+
+        var progressCalledCount2 = 0
+        var resultValue2: String?
+
+        var deinitTest2: NSObject? = NSObject()
+        weak var weakDeinitTest2 = deinitTest2
+
+        let expectation2 = expectationWithDescription("")
+
+        stream.observe { ev -> Void in
+
+            switch ev {
+            case .Success(let value):
+                if deinitTest2 != nil {
+                    deinitTest2 = nil
+                    resultValue2 = value
+                    expectation2.fulfill()
+                }
+            case .Failure:
+                XCTFail()
+            case .Progress(let progress):
+                XCTAssertEqual(progressCalledCount2, progress as? Int)
+                progressCalledCount2 += 1
+            }
+        }
+
+        XCTAssertNil(weakDeinitTest1)
+        XCTAssertNotNil(weakDeinitTest2)
+
+        waitForExpectationsWithTimeout(0.5, handler: nil)
+
+        XCTAssertNil(weakDeinitTest2)
+
+        XCTAssertEqual(0, progressCalledCount1)
+        XCTAssertEqual(5, progressCalledCount2)
+        XCTAssertNil(resultValue1)
+        XCTAssertEqual("ok", resultValue2)
+
+        XCTAssertEqual(2, numberOfObservers)
     }
 
     func testTwoObservsersDisposeSecond() {
     }
 
+    func testTwoObservsersDisposeSecondImmediately() {
+    }
+
     func testTwoObservsersDisposeBoth() {
+    }
+
+    func testTwoObservsersDisposeBothImmediately() {
     }
 
     func testPerformanceExample() {
