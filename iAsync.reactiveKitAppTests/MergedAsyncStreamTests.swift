8fc6d80d73e06ae8b22fd8fa84215b6ad8d98abb
vlg <gorbenko.vova@gmail.com>
2016-02-08 05:18:12 +0300

    implementing reactiveKit additions

--- iAsync.reactiveKitAppTests/MergedAsyncStreamTests.swift
+++ iAsync.reactiveKitAppTests/MergedAsyncStreamTests.swift
@@ -86,7 +86,7 @@ class MergedAsyncStreamTests: XCTestCase {
         XCTAssertNil(weakDeinitTest)
     }
 
-    func testNormalFinishStream() {
+    func testNormalFinishStreamWithSharedNextBuffer() {
 
         var progressCalledCount1 = 0
         var resultValue1: String?
@@ -735,4 +735,417 @@ class MergedAsyncStreamTests: XCTestCase {
 
         XCTAssertEqual(2, numberOfObservers1)
     }
+
+    func testNormalFinishStream() {
+
+        var progressCalledCount1 = 0
+        var resultValue1: String?
+        var nextValues1 = [Int]()
+
+        var progressCalledCount2 = 0
+        var resultValue2: String?
+        var nextValues2 = [Int]()
+
+        weak var weakDeinitTest: NSObject? = nil
+        weak var weakMerger: MergerType?
+
+        var getterCallsCount = 0
+        var setterResult: String?
+        var setterEventCallsCount = 0
+        var setterValueCallsCount = 0
+
+        autoreleasepool {
+
+            let deinitTest = NSObject()
+            weakDeinitTest = deinitTest
+
+            let merger = MergerType()
+            weakMerger = merger
+
+            let stream = merger.mergedStream({ testStream() }, key: "1", getter: { () -> AsyncEvent<String, Int, NSError>? in
+
+                getterCallsCount += 1
+                deinitTest.description
+                return .Next(-1)
+            }, setter: { event -> Void in
+
+                deinitTest.description
+
+                setterEventCallsCount += 1
+                switch event {
+                case .Success(let value):
+                    setterResult = value
+                    setterValueCallsCount += 1
+                default:
+                    break
+                }
+            })
+
+            let expectation1 = self.expectationWithDescription("")
+            let expectation2 = self.expectationWithDescription("")
+
+            stream.observe { ev -> Void in
+
+                deinitTest.description
+
+                switch ev {
+                case .Success(let value):
+                    resultValue1 = value
+                    expectation1.fulfill()
+                case .Failure:
+                    XCTFail()
+                case .Next(let next):
+                    nextValues1.append(next)
+                    if progressCalledCount1 == 0 {
+                        XCTAssertEqual(-1, next)
+                    } else {
+                        XCTAssertEqual(progressCalledCount1, next + 1)
+                    }
+                    progressCalledCount1 += 1
+                }
+            }
+            stream.observe { ev -> Void in
+
+                deinitTest.description
+
+                switch ev {
+                case .Success(let value):
+                    resultValue2 = value
+                    expectation2.fulfill()
+                case .Failure:
+                    XCTFail()
+                case .Next(let next):
+                    nextValues2.append(next)
+                    if progressCalledCount2 == 0 {
+                        XCTAssertEqual(-1, next)
+                    } else {
+                        XCTAssertEqual(progressCalledCount2, next + 1)
+                    }
+                    progressCalledCount2 += 1
+                }
+            }
+
+            XCTAssertNotNil(weakDeinitTest)
+
+            self.waitForExpectationsWithTimeout(0.5, handler: nil)
+        }
+
+        XCTAssertNil(weakDeinitTest)
+        XCTAssertNil(weakMerger)
+
+        XCTAssertEqual(2, getterCallsCount)
+        XCTAssertEqual(setterResult, "ok")
+        XCTAssertEqual(6, setterEventCallsCount)
+        XCTAssertEqual(1, setterValueCallsCount)
+
+        XCTAssertEqual(6, progressCalledCount1)
+        XCTAssertEqual([-1, 0, 1, 2, 3, 4], nextValues1)
+        XCTAssertEqual("ok", resultValue1)
+
+        XCTAssertEqual(6, progressCalledCount2)
+        XCTAssertEqual([-1, 0, 1, 2, 3, 4], nextValues2)
+        XCTAssertEqual("ok", resultValue2)
+
+        XCTAssertEqual(numberOfObservers1, 1)
+    }
+
+    func testNormalFinishStreamFactoryCountCalls() {
+
+        var progressCalledCount1 = 0
+        var resultValue1: String?
+        var nextValues1 = [Int]()
+
+        var progressCalledCount2 = 0
+        var resultValue2: String?
+        var nextValues2 = [Int]()
+
+        weak var weakDeinitTest: NSObject? = nil
+        weak var weakMerger: MergerType?
+
+        var getterCallsCount = 0
+        var setterResult: String?
+        var setterEventCallsCount = 0
+        var setterValueCallsCount = 0
+        var factoryCountCalls = 0
+
+        autoreleasepool {
+
+            let deinitTest = NSObject()
+            weakDeinitTest = deinitTest
+
+            let merger = MergerType()
+            weakMerger = merger
+
+            let createStream = { (force: Bool) -> AsyncStream<String, Int, NSError> in
+
+                return merger.mergedStream({ () -> AsyncStream<String, Int, NSError> in
+                    factoryCountCalls += 1
+                    return testStream()
+                }, key: "1", getter: { () -> AsyncEvent<String, Int, NSError>? in
+
+                    getterCallsCount += 1
+                    deinitTest.description
+                    //return .Success("sucess-ok")
+                    return .Next(-1)
+                }, setter: { event -> Void in
+
+                    deinitTest.description
+
+                    setterEventCallsCount += 1
+                    switch event {
+                    case .Success(let value):
+                        setterResult = value
+                        setterValueCallsCount += 1
+                    default:
+                        break
+                    }
+                })
+            }
+
+            let stream1 = createStream(false)
+            let stream2 = createStream(false)
+
+            let expectation1 = self.expectationWithDescription("")
+            let expectation2 = self.expectationWithDescription("")
+
+            stream1.observe { ev -> Void in
+
+                deinitTest.description
+
+                switch ev {
+                case .Success(let value):
+                    resultValue1 = value
+                    expectation1.fulfill()
+                case .Failure:
+                    XCTFail()
+                case .Next(let next):
+                    nextValues1.append(next)
+                    if progressCalledCount1 == 0 {
+                        XCTAssertEqual(-1, next)
+                    } else {
+                        XCTAssertEqual(progressCalledCount1, next + 1)
+                    }
+                    progressCalledCount1 += 1
+                }
+            }
+            stream2.observe { ev -> Void in
+
+                deinitTest.description
+
+                switch ev {
+                case .Success(let value):
+                    resultValue2 = value
+                    expectation2.fulfill()
+                case .Failure:
+                    XCTFail()
+                case .Next(let next):
+                    nextValues2.append(next)
+                    if progressCalledCount2 == 0 {
+                        XCTAssertEqual(-1, next)
+                    } else {
+                        XCTAssertEqual(progressCalledCount2, next + 1)
+                    }
+                    progressCalledCount2 += 1
+                }
+            }
+
+            XCTAssertNotNil(weakDeinitTest)
+
+            self.waitForExpectationsWithTimeout(0.5, handler: nil)
+        }
+
+        XCTAssertNil(weakDeinitTest)
+        XCTAssertNil(weakMerger)
+
+        XCTAssertEqual(2, getterCallsCount)
+        XCTAssertEqual(setterResult, "ok")
+        XCTAssertEqual(6, setterEventCallsCount)
+        XCTAssertEqual(1, setterValueCallsCount)
+
+        XCTAssertEqual(6, progressCalledCount1)
+        XCTAssertEqual([-1, 0, 1, 2, 3, 4], nextValues1)
+        XCTAssertEqual("ok", resultValue1)
+
+        XCTAssertEqual(6, progressCalledCount2)
+        XCTAssertEqual([-1, 0, 1, 2, 3, 4], nextValues2)
+        XCTAssertEqual("ok", resultValue2)
+
+        XCTAssertEqual(factoryCountCalls, 1)
+        XCTAssertEqual(numberOfObservers1, 1)
+    }
+
+    func testForceLoad() {
+
+        var progressCalledCount0 = 0
+        var resultValue0: String?
+        var nextValues0 = [Int]()
+
+        var progressCalledCount1 = 0
+        var resultValue1: String?
+        var nextValues1 = [Int]()
+
+        var progressCalledCount2 = 0
+        var resultValue2: String?
+        var nextValues2 = [Int]()
+
+        var progressCalledCount3 = 0
+        var resultValue3: String?
+        var nextValues3 = [Int]()
+
+        weak var weakDeinitTest: NSObject? = nil
+        weak var weakMerger: MergerType?
+
+        var getterCallsCount = 0
+        var setterResult: String?
+        var setterEventCallsCount = 0
+        var setterValueCallsCount = 0
+        var factoryCountCalls = 0
+
+        autoreleasepool {
+
+            let deinitTest = NSObject()
+            weakDeinitTest = deinitTest
+
+            let merger = MergerType()
+            weakMerger = merger
+
+            let createStream = { (force: Bool) -> AsyncStream<String, Int, NSError> in
+
+                return merger.mergedStream({ () -> AsyncStream<String, Int, NSError> in
+                    factoryCountCalls += 1
+                    return testStream()
+                }, key: "1", getter: { () -> AsyncEvent<String, Int, NSError>? in
+
+                    getterCallsCount += 1
+
+                    if force {
+                        return nil
+                    }
+
+                    deinitTest.description
+                    return .Success("sucess-ok")
+                }, setter: { event -> Void in
+
+                    deinitTest.description
+
+                    setterEventCallsCount += 1
+                    switch event {
+                    case .Success(let value):
+                        setterResult = value
+                        setterValueCallsCount += 1
+                    default:
+                        break
+                    }
+                })
+            }
+
+            let stream0 = createStream(false)
+            let stream1 = createStream(true)
+            let stream2 = createStream(true)
+            let stream3 = createStream(false)
+
+            let expectation0 = self.expectationWithDescription("")
+            let expectation1 = self.expectationWithDescription("")
+            let expectation2 = self.expectationWithDescription("")
+            let expectation3 = self.expectationWithDescription("")
+
+            stream0.observe { ev -> Void in
+
+                deinitTest.description
+
+                switch ev {
+                case .Success(let value):
+                    resultValue0 = value
+                    expectation0.fulfill()
+                case .Failure:
+                    XCTFail()
+                case .Next(let next):
+                    nextValues0.append(next)
+                    XCTAssertEqual(progressCalledCount0, next)
+                    progressCalledCount0 += 1
+                }
+            }
+            stream1.observe { ev -> Void in
+
+                deinitTest.description
+
+                switch ev {
+                case .Success(let value):
+                    resultValue1 = value
+                    expectation1.fulfill()
+                case .Failure:
+                    XCTFail()
+                case .Next(let next):
+                    nextValues1.append(next)
+                    XCTAssertEqual(progressCalledCount1, next)
+                    progressCalledCount1 += 1
+                }
+            }
+            stream2.observe { ev -> Void in
+
+                deinitTest.description
+
+                switch ev {
+                case .Success(let value):
+                    resultValue2 = value
+                    expectation2.fulfill()
+                case .Failure:
+                    XCTFail()
+                case .Next(let next):
+                    nextValues2.append(next)
+                    XCTAssertEqual(progressCalledCount2, next)
+                    progressCalledCount2 += 1
+                }
+            }
+            stream3.observe { ev -> Void in
+
+                deinitTest.description
+
+                switch ev {
+                case .Success(let value):
+                    resultValue3 = value
+                    expectation3.fulfill()
+                case .Failure:
+                    XCTFail()
+                case .Next(let next):
+                    nextValues3.append(next)
+                    XCTAssertEqual(progressCalledCount3, next)
+                    progressCalledCount3 += 1
+                }
+            }
+
+            XCTAssertNotNil(weakDeinitTest)
+
+            self.waitForExpectationsWithTimeout(0.5, handler: nil)
+        }
+
+        XCTAssertNil(weakDeinitTest)
+        XCTAssertNil(weakMerger)
+
+        XCTAssertEqual(4, getterCallsCount)
+        XCTAssertEqual(setterResult, "ok")
+        XCTAssertEqual(6, setterEventCallsCount)
+        XCTAssertEqual(1, setterValueCallsCount)
+
+        XCTAssertEqual(0, progressCalledCount0)
+        XCTAssertEqual([], nextValues0)
+        XCTAssertEqual("sucess-ok", resultValue0)
+
+        XCTAssertEqual(5, progressCalledCount1)
+        XCTAssertEqual([0, 1, 2, 3, 4], nextValues1)
+        XCTAssertEqual("ok", resultValue1)
+
+        XCTAssertEqual(5, progressCalledCount2)
+        XCTAssertEqual([0, 1, 2, 3, 4], nextValues2)
+        XCTAssertEqual("ok", resultValue2)
+
+        XCTAssertEqual(0, progressCalledCount3)
+        XCTAssertEqual([], nextValues3)
+        XCTAssertEqual("sucess-ok", resultValue3)
+
+        XCTAssertEqual(factoryCountCalls, 1)
+        XCTAssertEqual(numberOfObservers1, 1)
+    }
+
+    //TODO test dealloc merger while loading
 }
