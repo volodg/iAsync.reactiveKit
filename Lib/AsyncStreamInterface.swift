21820af51a65c7cee49b4e0341b3472c17e2973c
vlg <gorbenko.vova@gmail.com>
2016-02-09 09:26:44 +0300

    force freeing object on finish

--- Lib/AsyncStreamInterface.swift
+++ Lib/AsyncStreamInterface.swift
@@ -30,19 +30,27 @@ public func createStream<T: AsyncStreamInterface>(factory: () -> T) -> AsyncStre
 
         var observerHolder: (AsyncEvent<T.Value, T.Next, T.Error> -> ())? = observer
 
+        var objHolder: T? = nil
+
         let notifyOnce = { (event: AsyncEvent<T.Value, T.Next, T.Error>) -> () in
             guard let observer = observerHolder else { return }
+            objHolder      = nil
             observerHolder = nil
             observer(event)
         }
 
         let obj = factory()
+        objHolder = obj
 
         obj.asyncWithCallbacks(
             success: { notifyOnce(.Success($0))   },
             next   : { observerHolder?(.Next($0)) },
             error  : { notifyOnce(.Failure($0))   })
 
-        return BlockDisposable { observerHolder = nil; obj.cancel() }
+        return BlockDisposable {
+            observerHolder = nil
+            objHolder?.cancel()
+            objHolder = nil
+        }
     })
 }
