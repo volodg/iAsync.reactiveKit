331f63ddffddc59643796b12f11e6f8cb74150d7
vlg <gorbenko.vova@gmail.com>
2016-02-18 13:01:04 +0200

    implementing reactiveKit additions

---
+++ Lib/Extensions/AsyncStreamType+Additions.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncStreamType+Additions.swift
+//  iAsync_reactiveKit
+//
+//  Created by Gorbenko Vladimir on 18/02/16.
+//  Copyright (c) 2016 AppDaddy. All rights reserved.
+//
+
+import Foundation
+
+public extension AsyncStreamType where Error == NSError {
+
+    public func logError() -> AsyncStream<Value, Next, Error> {
+
+        return self.on(failure: { $0.writeErrorWithLogger() })
+    }
+}
