bb1457fc4bb688c6ea713eee8b49c8a4d9bc3454
vlg <gorbenko.vova@gmail.com>
2016-02-20 18:37:12 +0200

    implementing reactiveKit additions

--- Lib/Extensions/AsyncStreamType+Additions.swift
+++ Lib/Extensions/AsyncStreamType+Additions.swift
@@ -15,3 +15,11 @@ public extension AsyncStreamType where Error == NSError {
         return self.on(failure: { $0.writeErrorWithLogger() })
     }
 }
+
+public extension AsyncStreamType {
+
+    public func mapNext2AnyObject() -> AsyncStream<Value, AnyObject, Error> {
+
+        return mapNext { _ in NSNull() as AnyObject }
+    }
+}
